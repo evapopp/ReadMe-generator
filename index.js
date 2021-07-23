9fc50802944f33621af31501b654000e8f3e85a6
eva <83175995+evapopp@users.noreply.github.com>
2021-07-23 11:38:56 -0700

    uadded liscense info to markdown, updated questions, updated init function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -25,20 +26,55 @@ const questions = [
         message: 'Please enter your github username',
     },
     {
+        name: 'installation',
+        type: 'input',
+        message: 'Please indicate how to install application',
+    },
+    {
+        name: 'usage',
+        type: 'input',
+        message: 'Please indicate how this application will be used',
+    },
+    {
         name: 'license',
         type: 'list',
-        message: 'Please choose a licesnse from the list',
-        choices: '',
+        message: 'Please choose a license from the list',
+        choices: [
+            'Apache 2.0',
+            'Boost Software License 1.0',
+            'MIT',
+            'Mozilla Public License 2.0',
+            'Open Database License',
+        ],
+    },
+    {
+        name: 'contribution',
+        type: 'input',
+        message: 'Please identify the contributers of the application',
+    },
+    {
+        name: 'tests',
+        type: 'input',
+        message: 'Please indicate how to test that the application is working',
     },
 ];
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), (err) => err ? econsole.error(err) : console.log('README file generated!')
+    );
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    
+    inquirer.prompt(questions) 
+    .then((response => {
+     console.log('answer logged!')   
+     writeToFile( 'readme.md', response)
+    })
+    )
+
 }
 // ask questions
 // assemble answers
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,11 +1,20 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const badges = {
+    'Apache 2.0': '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+    'Boost Software License 1.0': '[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)',
+    'MIT': '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+    'Mozilla Public License 2.0': '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)',
+    'Open Database License': '[![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)](https://opendatacommons.org/licenses/odbl/)'
+  };
+  return badges[license]
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  return `##${liscense}`
+  return `##${license}`
 }
 
 // TODO: Create a function that returns the license section of README
@@ -15,7 +24,40 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-  ${renderLicenseLink(data.license)}
+  ## Table of Contents:
+  1. Description(#description)
+  2. Questions(#questions)
+  3. Installation(#installation)
+  4. Usage(#usage)
+  5. License(#license)
+  6. Contributing(#contributing)
+  7. Tests(#tests)
+
+
+  ## Description
+  ${data.description}
+
+  ## Questions
+  If you need to reach me with additional questions, contact me via: 
+  * email: ${data.email}
+  * github: ${data.github}
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${renderLicenseBadge(data.license)}
+
+  ## Contributing 
+  ${data.contribution}
+
+  ## Tests
+  ${data.tests}
+
+  
 `;
 }
 
